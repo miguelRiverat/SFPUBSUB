b5faaae5d3c91ace84bd6fff3bcbc730fc96e7e8
miguel <miguel@debian.baxten>
2019-08-19 23:35:19 -0500

    dates

--- utils.js
+++ utils.js
@@ -3,7 +3,11 @@
 let dates = dates => {
   let from = new Date(dates)
   from.setMonth(from.getMonth()-3)
+  from.setHours(12)
+  from.setDate(1)
   let to = new Date(dates)
+  to.setHours(12)
+  to.setDate(1)
   return [from.toISOString().split("T")[0], to.toISOString().split("T")[0]]
 }
 
